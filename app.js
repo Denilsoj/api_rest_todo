0fa608511ee8e7c71845ea9de22b5cd7fd7a8447
Denilson Oliveira <denilso-oliveira@pop-os.localdomain>
2024-01-22 14:57:27 -0300

    one commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  routes() {
+    this.app.use();
+  }
+}
+
+export default new App().app;
